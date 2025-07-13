9abd958eeed56be67b62e2e0acb4db6d57a1e6a8
Jack Tubby <jack200034@hotmail.com>
2025-07-14 00:34:27 +0100

    Setup flow of rate limiter and testing express app (move to module later)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
-import { NextFunction, Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express"; // remove express once testing is setup
 import { RateLimiterOptions } from "./types";
+import RateLimiter from "./limiter";
+import MemoryStore from "./store/memory";
 
 export function createRateLimiter(options: RateLimiterOptions) {
-  return function rateLimiter(req: Request, res: Response, next: NextFunction) {
-    next();
+  const store = options.store || new MemoryStore();
+  const limiter = new RateLimiter(options, store);
+
+  return async function rateLimiter(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const ipAddress = req.ip || "";
+    if (!ipAddress) {
+      return next();
+    }
+
+    const canProceed = await limiter.checkLimit(ipAddress);
+
+    if (canProceed) {
+      next();
+    } else {
+      return res.status(429).json({ message: "Too many requests" });
+    }
   };
 }
+
+const app = express();
+app.use(createRateLimiter({}));
+app.get("/", (req, res) => res.send("OK"));
+app.listen(3000);
