14d5bd8b99d46d44f6923407686184268dfb3548
Jack Tubby <jack200034@hotmail.com>
2025-07-14 20:50:41 +0100

    Implement token bucket logic

--- src/limiter.ts
+++ src/limiter.ts
@@ -1,21 +1,98 @@
 import { BucketState } from "./types";
 
 class RateLimiter {
-  ipAddress: string;
-  lastRefill: number;
+  ipAddress: string | null;
   timeFrame: number;
+  lastRefill!: number;
   store: any;
+  tokenLimit: number;
+  bucketStore: BucketState;
+  usersBucket: any;
 
   constructor(options: any, store: any) {
-    this.ipAddress = options.ipAddress;
-    this.timeFrame = options.timeFrame || 900;
+    this.timeFrame = options.timeFrame || 900000; // milliseconds (15 mins)
     this.store = store;
+    this.tokenLimit = options.tokenLimit || 50;
+    this.bucketStore = {
+      tokens: 0,
+      lastRefill: 0,
+    };
+    this.ipAddress = "";
   }
 
   public async checkLimit(ipAddress: string) {
-    const tokenCount = await this.store.get(ipAddress);
-    console.log(tokenCount);
+    this.ipAddress = ipAddress;
+    this.usersBucket = await this.store.get(ipAddress);
+    console.log("Bucket: ", this.usersBucket);
+    // if le bucket does not exist
+    if (!this.usersBucket) {
+      const res = await this.handleBucketNotExisting();
+      return res;
+    }
+    // if le bucket does exist
+    else {
+      await this.handleBucketRefill();
+      if (this.usersBucket.tokens === 0) {
+        const res = await this.handleNoTokensLeft();
+        return res;
+      } else {
+        const res = await this.handleBucketExisting();
+        return res;
+      }
+    }
+  }
+
+  public async handleBucketRefill() {
+    console.log("Bucket refill handle...");
+    this.lastRefill = this.usersBucket.lastRefill;
+    const now = new Date().getTime();
+    const timePassed = now - this.lastRefill;
+    const tokensToAdd = Math.floor(
+      (timePassed / this.timeFrame) * this.tokenLimit
+    );
+    const currentTokens = this.usersBucket.tokens;
+    const isOverspill =
+      currentTokens + tokensToAdd > this.tokenLimit ? true : false;
+    this.bucketStore = {
+      tokens: isOverspill ? this.tokenLimit : currentTokens + tokensToAdd,
+      lastRefill: new Date().getTime(),
+    };
+    await this.store.set(this.ipAddress, this.bucketStore);
+    this.usersBucket = {
+      ...this.usersBucket,
+      tokens: isOverspill ? this.tokenLimit : currentTokens + tokensToAdd,
+    };
+    console.log("bucket refill complete: ", this.usersBucket);
+  }
+
+  private async handleBucketNotExisting() {
+    console.log("Bucket does not exist");
+    this.bucketStore = {
+      tokens: this.tokenLimit - 1,
+      lastRefill: new Date().getTime(),
+    };
+    await this.store.set(this.ipAddress, this.bucketStore);
     return true;
+  }
+
+  private async handleBucketExisting() {
+    console.log("Bucket does exist");
+    this.bucketStore = {
+      tokens: this.usersBucket.tokens - 1,
+      lastRefill: new Date().getTime(),
+    };
+    await this.store.set(this.ipAddress, this.bucketStore);
+    return true;
+  }
+
+  private async handleNoTokensLeft() {
+    console.log("Bucket has no tokens left");
+    this.bucketStore = {
+      tokens: 0,
+      lastRefill: new Date().getTime(),
+    };
+    await this.store.set(this.ipAddress, this.bucketStore);
+    return false;
   }
 }
 
