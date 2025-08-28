9a717500dc2fbc822ecadec0526ef62347121b20
Jack Tubby <jack200034@hotmail.com>
2025-08-28 23:01:24 +0100

    Format document

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,36 @@
-import { NextFunction, Request, Response } from "express";
-import { RateLimiterOptions, StoreType } from "./types";
-import MemoryStore from "./store/memory";
-import CleanUp from "./cleanup";
-import RateLimiterFactory from "./limiter";
+import { NextFunction, Request, Response } from 'express'
+import { RateLimiterOptions, StoreType } from './types'
+import MemoryStore from './store/memory'
+import CleanUp from './cleanup'
+import RateLimiterFactory from './limiter'
 
 export function createRateLimiter(options: RateLimiterOptions) {
-  const store: StoreType = options.storeType || 'memory';
-  const limiter = RateLimiterFactory.create("tokenBucket", options, store); // Todo: default tokenBucket but can choose algo of choice
+  const store: StoreType = options.storeType || 'memory'
+  const limiter = RateLimiterFactory.create('tokenBucket', options, store) // Todo: default tokenBucket but can choose algo of choice
   if (options.enableCleanup !== false) {
-    startPeriodicCleanup(store, options.cleanupInterval);
+    startPeriodicCleanup(store, options.cleanupInterval)
   }
 
-  return async function rateLimiter(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
-    const key = req.ip || "";
+  return async function rateLimiter(req: Request, res: Response, next: NextFunction) {
+    const key = req.ip || ''
     if (!key) {
-      return next();
+      return next()
     }
-    console.log("incoming ip address: ", key);
+    console.log('incoming ip address: ', key)
 
-    const canProceed = await limiter.checkLimit(key);
+    const canProceed = await limiter.checkLimit(key)
 
     if (canProceed.success) {
-      console.log("Next...");
-      next();
+      console.log('Next...')
+      next()
     } else {
-      console.log("Too many requests");
-      return res.status(429).json({ message: "Too many requests" });
+      console.log('Too many requests')
+      return res.status(429).json({ message: 'Too many requests' })
     }
-  };
+  }
 }
 
 async function startPeriodicCleanup(store: any, interval: number = 3600000) {
-  const cleanup = new CleanUp(store, interval);
-  cleanup.scheduleCleanup();
+  const cleanup = new CleanUp(store, interval)
+  cleanup.scheduleCleanup()
 }
--- src/store/memory.ts
+++ src/store/memory.ts
@@ -1,28 +1,28 @@
-import { BucketState } from "../types";
+import { BucketState } from '../types'
 interface Store {
-  get(key: string): Promise<BucketState | null>;
-  set(key: string, state: BucketState): Promise<void>;
+  get(key: string): Promise<BucketState | null>
+  set(key: string, state: BucketState): Promise<void>
 }
 
 class MemoryStore implements Store {
-	private buckets = new Map<string, BucketState>();
+  private buckets = new Map<string, BucketState>()
 
-	async get(key: string): Promise<BucketState | null> {
-		console.log("Bucket: ", this.buckets.get(key)); 
-		return this.buckets.get(key) || null;
-	}
+  async get(key: string): Promise<BucketState | null> {
+    console.log('Bucket: ', this.buckets.get(key))
+    return this.buckets.get(key) || null
+  }
 
-	async set(key: string, state: BucketState): Promise<void> {
-		this.buckets.set(key, state);
-	}
+  async set(key: string, state: BucketState): Promise<void> {
+    this.buckets.set(key, state)
+  }
 
-	async delete(key: string) {
-		this.buckets.delete(key);
-	}
+  async delete(key: string) {
+    this.buckets.delete(key)
+  }
 
-	async getAll(): Promise<any | null> {
-		return this.buckets;
-	}
+  async getAll(): Promise<any | null> {
+    return this.buckets
+  }
 }
 
-export default MemoryStore;
+export default MemoryStore
