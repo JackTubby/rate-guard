1e717e2f4a49342e09213d3f2df01d859bf33043
Jack Tubby <jack200034@hotmail.com>
2025-11-27 14:01:57 +0000

    Refactor RateLimiterOptions interface for improved clarity and documentation

--- types/types.ts
+++ types/types.ts
@@ -1,15 +1,16 @@
 export interface RateLimiterOptions {
-  store?: BucketState | null;
-  cleanupInterval?: number;
-  enableCleanup?: boolean;
-  timeFrame?: number;
-  tokenLimit?: number;
-  value?: string;
-  status?: number;
-  message?: any;
-  storeType: string;
-  type: string;
+  store?: BucketState | null; // store - optional - handles store instance for redis
+  cleanupInterval?: number; // cleanupInterval - optional - clean up interval timing
+  enableCleanup?: boolean; // enableCleanup - optional - allow clean up operation
+  timeFrame?: number; // timeFrame - optional - window length
+  tokenLimit?: number; // tokenLimit - optional - allowed tokens within the window
+  value?: string; // value - optional - value to use as the key optional as defaults to ip
+  message?: any; // message - optional - an optional message to return to users who exceed limit
+  storeType: StoreTypes; // storeType - required - type of storage to use
+  type: string; // type - required - the type of algo to use
 }
+
+type StoreTypes = 'redis' | 'memory';
 
 export interface BucketState {
   tokens: number;
