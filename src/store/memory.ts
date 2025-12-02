91ce56bf8994ec51dcc1dfbc5dc563738d720899
Jack Tubby <jack200034@hotmail.com>
2025-12-02 22:16:56 +0000

    Indentation fix

--- src/store/memory.ts
+++ src/store/memory.ts
@@ -5,23 +5,23 @@ interface Store {
 }
 
 class MemoryStore implements Store {
-	private buckets = new Map<string, BucketState>();
+  private buckets = new Map<string, BucketState>();
 
-	async get(key: string): Promise<BucketState | null> {
-		return this.buckets.get(key) || null;
-	}
+  async get(key: string): Promise<BucketState | null> {
+    return this.buckets.get(key) || null;
+  }
 
-	async set(key: string, state: BucketState): Promise<void> {
-		this.buckets.set(key, state);
-	}
+  async set(key: string, state: BucketState): Promise<void> {
+    this.buckets.set(key, state);
+  }
 
-	async delete(key: string) {
-		this.buckets.delete(key);
-	}
+  async delete(key: string) {
+    this.buckets.delete(key);
+  }
 
-	async getAll(): Promise<any | null> {
-		return this.buckets;
-	}
+  async getAll(): Promise<any | null> {
+    return this.buckets;
+  }
 }
 
 export default MemoryStore;
