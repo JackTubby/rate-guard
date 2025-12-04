05e3a85013e719e5dccd120def66c234eba4ba42
Jack Tubby <jack200034@hotmail.com>
2025-12-04 19:42:00 +0000

    Refactor criticalOptions helper

--- src/helpers/entry-helpers.ts
+++ src/helpers/entry-helpers.ts
@@ -2,18 +2,11 @@ import { RateLimiterOptions } from "../../types/types";
 import { RateGuardError } from "../errors/errors";
 import { Request } from "express";
 
-export function criticalOptions(options: RateLimiterOptions) {
+export function criticalOptions(options: RateLimiterOptions): string[] {
   if (!options) return ["options missing"];
-  const criticalOptions = ["type", "storeType"];
-  const passedOptions = Object.keys(options);
-  let missedOptions: string[] = [];
-  criticalOptions.forEach((i) => {
-    const b = passedOptions.includes(i);
-    if (!b) {
-      missedOptions.push(i);
-    }
-  });
-  return missedOptions;
+  
+  const required = ["type", "storeType"];
+  return required.filter(opt => !(opt in options));
 }
 
 export function resolveRateLimitKey(
